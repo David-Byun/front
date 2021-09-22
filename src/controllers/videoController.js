99d934d00fe324c458ef27e7366905a737c9ce06
David-Byun <byundavid@naver.com>
2021-09-22 13:13:15 +0900

    delete comments

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -11,7 +11,8 @@ export const home = async (req, res) => {
 
 export const watch = async (req, res) => {
   const { id } = req.params
-  const video = await Video.findById(id).populate('owner')
+  const video = await Video.findById(id).populate('owner').populate('comments')
+  console.log(video)
   if (!video) {
     return res.render('404', { pageTitle: 'Video not found.' })
   }
@@ -139,5 +140,23 @@ export const createComment = async (req, res) => {
     owner: user._id,
     video: id
   })
-  return res.sendStatus(201)
+  video.comments.push(comment._id)
+  video.save()
+  return res.status(201).json({ newCommentId: comment._id })
+}
+
+export const deleteComment = async (req, res) => {
+  const {
+    params: { id },
+    session: { user }
+  } = req
+  const comment = await Comment.findById(id).populate('owner')
+  const video = await Video.findById(comment.video)
+  if (String(comment.owner._id) !== String(user._id)) {
+    return res.sendStatus(404)
+  }
+  await Comment.findByIdAndRemove(id)
+  video.comments.remove(id)
+  video.save()
+  return res.sendStatus(200)
 }
