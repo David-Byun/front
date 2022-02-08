81c360421bf53af95434476f0c62efff5e58ef03
David-Byun <byundavid@naver.com>
2022-02-08 22:57:06 +0900

    Custom App

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styles from "./NavBar.module.css";
 
 export default function NavBar() {
   const router = useRouter();
@@ -13,14 +12,11 @@ export default function NavBar() {
         <a className={router.pathname === "/about" ? "active" : ""}>About</a>
       </Link>
       <style jsx>{`
-        nav {
-          background-color: tomato;
-        }
         a {
           text-decoration: none;
         }
         .active {
-          color: blue;
+          color: tomato;
         }
       `}</style>
     </nav>
