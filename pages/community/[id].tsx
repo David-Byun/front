b4ee9fe5031bb8a1e4287260139891dae9b801c4
David-Byun <byundavid@naver.com>
2022-03-01 12:21:19 +0900

    Community Detail

---
+++ pages/community/[id].tsx
@@ -0,0 +1,88 @@
+import type { NextPage } from "next";
+
+const CommunityPostDetail: NextPage = () => {
+  return (
+    <div>
+      <span className="my-2.5 inline-flex items-center rounded-full bg-gray-100 px-2.5 py-0.5 text-xs font-medium text-gray-900">
+        동네질문
+      </span>
+      <div className="mb-3 flex cursor-pointer items-center space-x-3 border-b px-4 py-3">
+        <div className="h-10 w-10 rounded-full bg-slate-300" />
+        <div>
+          <p className="text-sm font-medium text-gray-700">Steve Jebs</p>
+          <p className="text-xs font-medium text-gray-500">
+            View profile &rarr;
+          </p>
+        </div>
+      </div>
+
+      <div className="mt-2 px-2.5 text-gray-700">
+        <span className="font-medium text-orange-500">Q.</span> What is the best
+        restaurant?
+      </div>
+      <div className="mt-3 flex w-full space-x-5 border-t border-b-[1.5px] px-3 py-2.5 text-gray-700">
+        <span className="flex items-center space-x-2 text-sm">
+          <svg
+            className="h-4 w-4"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+            ></path>
+          </svg>
+          <span>궁금해요 1</span>
+        </span>
+        <span className="flex items-center space-x-2 text-sm">
+          <svg
+            className="h-4 w-4"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+            ></path>
+          </svg>
+          <span>답변 1</span>
+        </span>
+      </div>
+      <div className="my-5 space-y-5 px-4">
+        <div className="flex items-start space-x-3">
+          <div className="h-8 w-8 rounded-full bg-slate-400" />
+          <div>
+            <span className="block text-sm font-medium text-gray-700">
+              Steve Jebs
+            </span>
+            <span className="block text-xs text-gray-500">2시간 전</span>
+            <p className="mt-2 text-gray-700">
+              The best mandu restaurant is the one to the next my house
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="px-3">
+        <textarea
+          rows={4}
+          className="mt-1 w-full rounded-md border-gray-300 shadow-sm focus:border-orange-500 focus:ring-orange-500"
+          placeholder="Answer this question"
+        />
+      </div>
+      <button className="mt-2 w-full flex-1 rounded-md bg-orange-500 py-3 font-medium text-white hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-offset-2">
+        Reply
+      </button>
+    </div>
+  );
+};
+
+export default CommunityPostDetail;
