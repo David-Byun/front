b35e0b4b744bb0d9162190ae9d9ba18538af68c8
David-Byun <byundavid@naver.com>
2022-03-09 09:28:29 +0900

    #5.17 Layout part Two

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { cls } from "../libs/utils";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 interface LayoutProps {
   title?: string;
@@ -14,19 +16,126 @@ export default function Layout({
   hasTabBar,
   children,
 }: LayoutProps) {
+  const router = useRouter();
+  const onClick = () => {
+    router.back();
+  };
   return (
     <div>
-      <div className="fixed top-0 flex w-full items-center justify-center border-b bg-white py-3 text-lg font-medium text-orange-700">
+      <div
+        className={cls(
+          !canGoBack ? "justify-center" : "",
+          "fixed top-0 flex w-full max-w-xl items-center border-b bg-white px-1 py-3 text-lg font-medium text-orange-700"
+        )}
+      >
+        {canGoBack ? (
+          <button onClick={onClick}>
+            <svg
+              className="h-6 w-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M15 19l-7-7 7-7"
+              ></path>
+            </svg>
+          </button>
+        ) : null}
         {title ? <span>{title}</span> : null}
       </div>
-      <div className={cls("pt-10", hasTabBar ? "pb-10" : "")}>{children}</div>
+      <div className={cls("pt-14", hasTabBar ? "pb-24" : "")}>{children}</div>
       {hasTabBar ? (
-        <nav className="fixed bottom-0 flex items-center justify-between border-t bg-white pb-10 pt-3 text-gray-800">
-          <span>d</span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
+        <nav className="fixed bottom-0 flex w-full max-w-xl items-center justify-between border-t bg-white px-10 pb-10 pt-3 text-xs text-gray-800">
+          <Link href="/">
+            <a className="flex flex-col items-center space-y-2">
+              <svg
+                className="h-6 w-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+                ></path>
+              </svg>
+              <span>홈</span>
+            </a>
+          </Link>
+          <Link href="/community">
+            <a className="flex flex-col items-center space-y-2">
+              <svg
+                className="h-6 w-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z"
+                ></path>
+              </svg>
+              <span>동네생활</span>
+            </a>
+          </Link>
+          <Link href="/chats">
+            <a className="flex flex-col items-center space-y-2">
+              <svg
+                className="h-6 w-6"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+              <span>채팅</span>
+            </a>
+          </Link>
+          <Link href="/live">
+            <a className="flex flex-col items-center space-y-2">
+              <svg
+                className="h-6 w-6"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path>
+              </svg>
+              <span>라이브</span>
+            </a>
+          </Link>
+          <Link href="/profile">
+            <a className="flex flex-col items-center space-y-2">
+              <svg
+                className="h-6 w-6"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+              <span>나의 캐럿</span>
+            </a>
+          </Link>
         </nav>
       ) : null}
     </div>
--- pages/chats/index.tsx
+++ pages/chats/index.tsx
@@ -1,23 +1,26 @@
 import type { NextPage } from "next";
+import Layout from "../../components/layout";
 
 const Chats: NextPage = () => {
   return (
-    <div className="divide-y-[1px] py-10">
-      {[1, 1, 1, 1, 1, 1, 1].map((_, i) => (
-        <div
-          key={i}
-          className="mb-3 flex cursor-pointer items-center space-x-3 px-4 py-2 "
-        >
-          <div className="h-10 w-10 rounded-full bg-slate-300 px-4" />
-          <div>
-            <p className=" text-gray-700">Steve Jebs</p>
-            <p className="text-sm font-medium text-gray-500">
-              See you tomorrow in the corner at 2pm!
-            </p>
+    <Layout title="Chats" hasTabBar>
+      <div className="divide-y-[1px] py-10">
+        {[1, 1, 1, 1, 1, 1, 1].map((_, i) => (
+          <div
+            key={i}
+            className="mb-3 flex cursor-pointer items-center space-x-3 px-4 py-2 "
+          >
+            <div className="h-10 w-10 rounded-full bg-slate-300 px-4" />
+            <div>
+              <p className=" text-gray-700">Steve Jebs</p>
+              <p className="text-sm font-medium text-gray-500">
+                See you tomorrow in the corner at 2pm!
+              </p>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </Layout>
   );
 };
 
