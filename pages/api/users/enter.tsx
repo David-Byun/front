d27186d3cf1733081b9ace00ddb5c320ba34cb79
David-Byun <byundavid@naver.com>
2022-03-13 15:35:07 +0900

    #9.2 Token Logic

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -7,16 +7,23 @@ import { apiResolver } from "next/dist/server/api-utils";
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { phone, email } = req.body;
   const payload = phone ? { phone: +phone } : { email };
-  const user = await client.user.upsert({
-    where: {
-      ...payload,
+  const token = await client.token.create({
+    data: {
+      payload: "123123",
+      user: {
+        connectOrCreate: {
+          where: {
+            ...payload,
+          },
+          create: {
+            name: "Anonymous",
+            ...payload,
+          },
+        },
+      },
     },
-    create: {
-      name: "Anonymous",
-      ...payload,
-    },
-    update: {},
   });
+  console.log(token);
   /* if (email) {
     user = await client.user.findUnique({
       where: {
@@ -51,7 +58,7 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
       });
     }
   } */
-  console.log(user);
+
   return res.status(200).end();
 }
 
