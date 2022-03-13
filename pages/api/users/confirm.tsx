c6a2c4db6e4f44ee32f22224d9015bad57563d8c
David-Byun <byundavid@naver.com>
2022-03-13 22:10:08 +0900

    #9.7 Serverless Sessions

--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -1,11 +1,17 @@
 import client from "@libs/server/client";
+import { withIronSessionApiRoute } from "iron-session/next";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { token } = req.body;
-  console.log(token);
+  const exists = await client.token.findUnique({ where: { payload: token } });
+  if (!exists) res.status(404).end();
+  console.log(exists);
   res.status(200).end();
 }
 
-export default withHandler("POST", handler);
+export default withIronSessionApiRoute(withHandler("POST", handler), {
+  cookieName: "carrotsession",
+  password: "2342342342342342sdfdsf342dsfdsfdsfsdfij23k4j23k4jkjk34",
+});
