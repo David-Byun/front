b86eeb2df3725973c85ee1dcb82e1b8568044372
David-Byun <byundavid@naver.com>
2022-03-13 10:35:17 +0900

    #9.1 Accounts Logic

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,10 +1,58 @@
+import client from "@libs/server/client";
 import withHandler from "@libs/server/withHandler";
+import { prisma } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
 import { apiResolver } from "next/dist/server/api-utils";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
-  return res.json({ ok: true });
+  const { phone, email } = req.body;
+  const payload = phone ? { phone: +phone } : { email };
+  const user = await client.user.upsert({
+    where: {
+      ...payload,
+    },
+    create: {
+      name: "Anonymous",
+      ...payload,
+    },
+    update: {},
+  });
+  /* if (email) {
+    user = await client.user.findUnique({
+      where: {
+        email,
+      }, 
+    });
+    if (user) console.log("found it");
+    if (!user) {
+      console.log("Did not find. Will create.");
+      user = await client.user.create({
+        data: {
+          name: "Anonymous",
+          email,
+        },
+      });
+    }
+  }
+  if (phone) {
+    user = await client.user.findUnique({
+      where: {
+        phone: +phone,
+      },
+    });
+    if (user) console.log("found it");
+    if (!user) {
+      console.log("Did not find. Will create.");
+      user = await client.user.create({
+        data: {
+          name: "Anonymous",
+          phone: +phone,
+        },
+      });
+    }
+  } */
+  console.log(user);
+  return res.status(200).end();
 }
 
 export default withHandler("POST", handler);
