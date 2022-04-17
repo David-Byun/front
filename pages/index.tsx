6f10af17f6d6dd2d8d2bbd15610d90c626a3d041
David-Byun <byundavid@naver.com>
2022-04-17 22:19:35 +0900

    #11.10 Counting Relationships

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,8 +21,7 @@ const Home: NextPage = () => {
             key={product.id}
             title={product.name}
             price={product.price}
-            comments={1}
-            hearts={1}
+            hearts={product._count.favs}
           />
         ))}
         <FloatingButton href="/products/upload">
