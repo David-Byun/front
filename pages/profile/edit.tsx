3681f500a95688df0c7be50c692916802b42de6d
David-Byun <byundavid@naver.com>
2022-05-01 12:45:20 +0900

    #13.5 Edit Profile part One

--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -1,49 +1,78 @@
+import Button from "@components/button";
+import Input from "@components/input";
+import Layout from "@components/layout";
+import userUser from "@libs/client/useUser";
 import type { NextPage } from "next";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+
+interface EditProfileForm {
+  email?: string;
+  phone?: string;
+  formErrors?: string;
+}
 
 const EditProfile: NextPage = () => {
+  const { user } = userUser();
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm<EditProfileForm>();
+  useEffect(() => {
+    if (user?.email) setValue("email", user.email);
+    if (user?.phone) setValue("phone", user.phone);
+  }, [user, setValue]);
+  const onValid = ({ email, phone }: EditProfileForm) => {
+    if (email === "" && phone === "") {
+      setError("formErrors", {
+        message: "Email or Phone number are required. You need to choose one.",
+      });
+    }
+  };
   return (
-    <div className="space-y-4 py-10 px-4">
-      <div className="flex items-center space-x-3">
-        <div className="h-14 w-14 rounded-full bg-slate-500" />
-        <label
-          htmlFor="picture"
-          className="cursor-pointer rounded-md border border-gray-300 py-2 px-3 text-sm font-medium text-gray-700 shadow-sm focus:ring-2 focus:ring-orange-500 focus:ring-offset-2"
-        >
-          Change
-          <input id="picture" type="file" className="hidden" accept="image/*" />
-        </label>
-      </div>
-      <div className="space-y-1">
-        <label htmlFor="email" className="text-sm font-medium text-gray-700">
-          Email address
-        </label>
-        <input
-          id="email"
-          type="email"
-          className="w-full appearance-none rounded-md border border-gray-400 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-orange-500 focus:outline-none  focus:ring-orange-500"
-          required
-        />
-      </div>
-      <div className="space-y-1">
-        <label htmlFor="phone" className="text-sm font-medium text-gray-700">
-          Phone number
-        </label>
-        <div className="flex rounded-md shadow-sm">
-          <span className="flex select-none items-center justify-center rounded-l-md border border-r-0 border-gray-300 bg-gray-50 px-3 text-sm text-gray-500">
-            +82
-          </span>
-          <input
-            id="phone"
-            type="text"
-            className="w-full appearance-none rounded-md border border-gray-400 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-orange-500 focus:outline-none  focus:ring-orange-500"
-            required
-          />
+    <Layout canGoBack title="Edit Profile">
+      <form onSubmit={handleSubmit(onValid)} className="space-y-4 py-10 px-4">
+        <div className="flex items-center space-x-3">
+          <div className="h-14 w-14 rounded-full bg-slate-500" />
+          <label
+            htmlFor="picture"
+            className="cursor-pointer rounded-md border border-gray-300 py-2 px-3 text-sm font-medium text-gray-700 shadow-sm focus:ring-2 focus:ring-orange-500 focus:ring-offset-2"
+          >
+            Change
+            <input
+              id="picture"
+              type="file"
+              className="hidden"
+              accept="image/*"
+            />
+          </label>
         </div>
-      </div>
-      <button className="text-small mt-6 w-full rounded-md border border-transparent bg-orange-500 py-2 px-4 font-medium text-white shadow-sm hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2">
-        Update Profile
-      </button>
-    </div>
+        <Input
+          register={register("email")}
+          required={false}
+          label="Email address"
+          name="email"
+          type="email"
+        />
+        <Input
+          register={register("phone")}
+          required={false}
+          label="Phone number"
+          name="phone"
+          type="text"
+          kind="phone"
+        />
+        {errors.formErrors ? (
+          <span className="my-2 block text-center font-medium text-red-500">
+            {errors.formErrors.message}
+          </span>
+        ) : null}
+        <Button text="Update profile" />
+      </form>
+    </Layout>
   );
 };
 
