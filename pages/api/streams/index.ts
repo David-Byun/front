334093c32288f7fcb202e21a88579ce8151fbe08
David-Byun <byundavid@naver.com>
2022-05-01 20:41:49 +0900

    #15.1 Image Preview

--- pages/api/streams/index.ts
+++ pages/api/streams/index.ts
@@ -21,7 +21,10 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
     });
     res.json({ ok: true, stream });
   } else if (req.method === "GET") {
-    const streams = await client.stream.findMany({});
+    const streams = await client.stream.findMany({
+      take: 10,
+      skip: 10,
+    });
     res.json({ ok: true, streams });
   }
 }
