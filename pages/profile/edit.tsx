6593f80acf06d96ed275a4e28c1f2661eb25b99c
David-Byun <byundavid@naver.com>
2022-05-07 14:36:08 +0900

    #15.4 Direct Upload URL

--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -37,18 +37,31 @@ const EditProfile: NextPage = () => {
   }, [user, setValue]);
   const [editProfile, { data, loading }] =
     useMutation<EditProfileResponse>(`/api/users/me`);
-  const onValid = ({ email, phone, name, avatar }: EditProfileForm) => {
+  const onValid = async({ email, phone, name, avatar }: EditProfileForm) => {
     if (loading) return;
     if (email === "" && phone === "" && name === "") {
       return setError("formErrors", {
         message: "Email or Phone number are required. You need to choose one.",
       });
     }
-    editProfile({
-      email,
-      phone,
-      name,
-    });
+    if (avatar && avatar.length > 0) {
+      // ask for CF URL
+      const cloudflareRequest = await (await fetch(`/api/files`)).json()
+      console.log(cloudflareRequest)
+      // upload file to CF URL
+      return
+      editProfile({
+        email,
+        phone,
+        name,
+        // avatarUrl : CF URL
+      });
+    } else {
+      editProfile({
+        email,
+        phone,
+        name,
+      });
   };
   useEffect(() => {
     if (data && !data.ok && data.error) {
