3d1761f035cb48b17a28048b2eaa99c5ddcbf6a5
David-Byun <byundavid@naver.com>
2022-07-03 14:45:26 +0900

    recap TokenUI

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,22 +1,56 @@
+import twilio from "twilio";
+import mail from "@sendgrid/mail";
 import client from "@libs/server/client";
 import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
 
-export default async function handler(
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+mail.setApiKey(process.env.SENDGRID_KEY!);
+
+async function handler(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse<ResponseType>
 ) {
   const { phone, email } = req.body;
-  const payLoad = phone ? { phone: +phone } : { email };
-  const user = await client.user.upsert({
-    where: {
-      ...payLoad,
-    },
-    update: {},
-    create: {
-      name: "Anonymous",
-      ...payLoad,
+  const user = phone ? { phone: +phone } : email ? { email } : null;
+  if (!user) return res.status(400).json({ ok: false });
+  const payload = Math.floor(100000 + Math.random() * 900000) + "";
+  const token = await client.token.create({
+    data: {
+      payload,
+      user: {
+        connectOrCreate: {
+          where: {
+            ...user,
+          },
+          create: {
+            name: "Anonymous",
+            ...user,
+          },
+        },
+      },
     },
   });
-  console.log(user);
-  return res.status(200).end();
+  /* if (phone) {
+    const message = await twilioClient.messages.create({
+      messagingServiceSid: process.env.TWILIO_MSID,
+      to: process.env.MY_PHONE!,
+      body: `Your login token is ${payload}`,
+    });
+    console.log(message);
+  } else if (email) {
+    const email = await mail.send({
+      from: "byundavid@naver.com",
+      to: "byundavid@naver.com",
+      subject: "Your Carrot Maret Verification Email",
+      text: `Your token is ${payload}`,
+      html: `<strong>Your token is ${payload}</strong>`,
+    });
+    console.log(email);
+  } */
+  return res.json({
+    ok: true,
+  });
 }
+
+export default withHandler("POST", handler);
