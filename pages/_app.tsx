7d07d87a7f3119705863b7432fe2cfa93d7c85eb
David-Byun <byundavid@naver.com>
2022-08-15 14:37:01 +0900

    challenge

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,17 +4,9 @@ import { SWRConfig } from "swr";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <SWRConfig
-      value={{
-        refreshInterval: 2000,
-        fetcher: (url: string) =>
-          fetch(url).then((response) => response.json()),
-      }}
-    >
-      <div className="mx-auto w-full max-w-xl">
-        <Component {...pageProps} />
-      </div>
-    </SWRConfig>
+    <div className="mx-auto w-full max-w-xl">
+      <Component {...pageProps} />
+    </div>
   );
 }
 
