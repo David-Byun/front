ff0555495be7bb449cbeed83fd47b5b0b7a1d769
David-Byun <byundavid@naver.com>
2022-08-27 16:52:41 +0900

    blockchan by ts

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,52 @@
 import crypto from "crypto";
 
-interface BlockChainModel {
+interface BlockShape {
+  hash: string;
   prevHash: string;
-  number: number;
+  height: number;
   data: string;
 }
 
-class Block implements BlockChainModel {
+class Block implements BlockShape {
   public hash: string;
   constructor(
     public prevHash: string,
-    public number: number,
+    public height: number,
     public data: string
   ) {
-    this.hash = Block.calculate(prevHash, number, data);
+    this.hash = Block.calculateHash(prevHash, height, data);
   }
-  static calculate(prevHash, number, data) {
-    const toHash = `${prevHash}${number}${data}`;
+  static calculateHash(prevHash: string, height: number, data: string) {
+    const toHash = `${prevHash}${height}${data}`;
     return crypto.createHash("sha256").update(toHash).digest("hex");
   }
 }
 
-class BlockChain {
+class Blockchain {
   private blocks: Block[];
   constructor() {
     this.blocks = [];
   }
+  private getPrevHash() {
+    if (this.blocks.length === 0) return "";
+    return this.blocks[this.blocks.length - 1].hash;
+  }
+  public addBlock(data: string) {
+    const newBlock = new Block(
+      this.getPrevHash(),
+      this.blocks.length + 1,
+      data
+    );
+    this.blocks.push(newBlock);
+  }
+  public getBlocks() {
+    return [...this.blocks];
+  }
 }
+
+const blockchain = new Blockchain();
+blockchain.addBlock("Fisrt one");
+blockchain.addBlock("Second one");
+blockchain.addBlock("Third one");
+
+console.log(blockchain.getBlocks());
