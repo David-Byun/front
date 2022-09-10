559571b14e65f19256d6d5fe4028af7786a68a5e
David-Byun <byundavid@naver.com>
2022-09-10 21:51:36 +0900

    19.10 getStaticProps Recap

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,24 +1,46 @@
 import Layout from "@components/layout";
 import { readdirSync, readFileSync } from "fs";
 import matter from "gray-matter";
+import { NextPage } from "next";
 
-export default function Blog() {
+interface Post {
+  title: string;
+  data: string;
+  category: string;
+}
+
+const Blog: NextPage<{ posts: Post[] }> = ({ posts }) => {
   return (
     <Layout title="Blog" seoTitle="Blog">
-      <h1 className="font-semibold text-lg">Latest Posts:</h1>
+      <h1 className="font-semibold text-lg text-center my-10 mt-5">
+        Latest Posts:
+      </h1>
       <ul>
-        <li>welcome everyone!</li>
+        {posts.map((post, index) => (
+          <div key={index} className="mb-5">
+            <span className="text-lg text-red-500">{post.title}</span>
+            <div>
+              <span>
+                {post.data} / {post.category}
+              </span>
+            </div>
+          </div>
+        ))}
       </ul>
     </Layout>
   );
-}
+};
 
 export async function getStaticProps() {
-  readdirSync("./posts").forEach((file) => {
+  const blogPosts = readdirSync("./posts").map((file) => {
     const content = readFileSync(`./posts/${file}`, "utf-8");
-    console.log(matter(content));
+    return matter(content).data;
   });
   return {
-    props: {},
+    props: {
+      posts: blogPosts,
+    },
   };
 }
+
+export default Blog;
---
+++ posts/02-my-trip-to-egypt.md
@@ -0,0 +1,9 @@
+---
+title: Egypt is greate!
+data: 2022.09.10
+category: travel
+---
+
+# Travel more. Buy less stuff.
+
+Get fat.
