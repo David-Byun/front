5d5b9b1755d6e7b63acfd4c7234bb42e128b8223
David-Byun <byundavid@naver.com>
2022-09-10 21:19:04 +0900

    Nwitter Auth

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import AppRouter from "./Router";
+import firebase from "../firebase";
+import { authService } from "../firebase";
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  return (
+    <>
+      <AppRouter isLoggedIn={isLoggedIn} />
+      <footer>&copy; Nwitter {new Date().getFullYear()}</footer>
+    </>
+  );
+}
+
+export default App;
