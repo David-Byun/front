062de7174ca4d09989f39b35085c1d026f89fa4e
David-Byun <byundavid@naver.com>
2022-09-12 11:07:17 +0900

    server component study

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,24 +1,20 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
 
-class CustomDocument extends Document {
-  render(): JSX.Element {
-    console.log("Document is running");
-    return (
-      <Html lang="ko">
-        <Head>
-          <link
-            href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;400&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
+export default function MyDocument() {
+  console.log("Document is running");
+  return (
+    <Html lang="ko">
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;400&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
 
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
-
-export default CustomDocument;
